e3e195584c347b0a60fb9dff3c76ab64d31a3f33
GG <jerometonnelier@yahoo.fr>
2020-11-16 13:57:36 +0100

    Swift 5.3 update

--- Sources/LoginWorkflow/Form/FormController.swift
+++ Sources/LoginWorkflow/Form/FormController.swift
@@ -308,7 +308,7 @@ public class FormController: UIViewController {
         case false:
             let errors = textFields.filter({ $0.text?.isEmpty ?? true == false && $0.isValid == false }).compactMap({ $0.validityString() })
             let format = ListFormatter()
-            guard let str = format.string(from: errors) ?? "", str.isEmpty == false else { return true }
+            guard let str = format.string(from: errors), str.isEmpty == false else { return true }
             showError(str)
             
         case true: updateNextButton()
